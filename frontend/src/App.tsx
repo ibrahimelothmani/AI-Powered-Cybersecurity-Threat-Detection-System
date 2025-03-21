73c5c7b31c1ad4d25f776317cbc1bc027b831cce
Ibrahim <ibrahimelothmani98@gmail.com>
2025-03-21 01:52:17 +0100

    fixe some issue in the interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import Box from '@mui/material/Box'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { AppBar, Toolbar, Typography, Button } from '@mui/material'
 import URLAnalyzer from './components/URLAnalyzer'
 import Login from './components/auth/Login'
@@ -28,34 +28,32 @@ const darkTheme = createTheme({
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Cybersecurity Analyzer
-              </Typography>
-              <Button color="inherit" component={Link} to="/">Home</Button>
-              <Button color="inherit" component={Link} to="/history">History</Button>
-              <Button color="inherit" component={Link} to="/contact">Contact</Button>
-              <Button color="inherit" component={Link} to="/login">Login</Button>
-              <Button color="inherit" component={Link} to="/register">Register</Button>
-            </Toolbar>
-          </AppBar>
-          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-            <Routes>
-              <Route path="/" element={<URLAnalyzer />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/history" element={<History />} />
-            </Routes>
-          </Box>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Cybersecurity Analyzer
+            </Typography>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/history">History</Button>
+            <Button color="inherit" component={Link} to="/contact">Contact</Button>
+            <Button color="inherit" component={Link} to="/login">Login</Button>
+            <Button color="inherit" component={Link} to="/register">Register</Button>
+          </Toolbar>
+        </AppBar>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Routes>
+            <Route path="/" element={<URLAnalyzer />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
         </Box>
-      </ThemeProvider>
-    </Router>
+      </Box>
+    </ThemeProvider>
   )
 }
 
